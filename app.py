6aeb9b2fe1825dcf470e51c71848bc85f0f0a830
Helen Du <59598171+helen2180@users.noreply.github.com>
2020-02-02 00:12:36 -0800

    changed app_helen.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import logging
 import time
 import edgeiq
-# Including if-statements
 
 def main():
     rightShoulder_y = 0
     leftShoulder_y = 0
-
-    # rightElbow_y = 0
-    # leftElbow_y = 0
 
     rightWrist_y = 0
     leftWrist_y = 0
@@ -35,41 +31,16 @@ def main():
 
                 text = ["Model: {}".format(pose_estimator.model_id)]
 
-                # Get right shoulder points
+                # Get points
                 for ind, pose in enumerate(results.poses):
-                    text.append(str("Right Shoulder Y"))
                     rightShoulder_y = pose.key_points[2][1]
-                    text.append(str(rightShoulder_y))
-
-                    text.append(str("Left Shoulder Y"))
                     leftShoulder_y = pose.key_points[5][1]
-                    text.append(str(leftShoulder_y))
-
-                    # text.append(str("Right Elbow Y"))
-                    # rightElbow_y = pose.key_points[3][1]
-                    # text.append(str(rightElbow_y))
-
-                    # text.append(str("Left Elbow Y"))
-                    # leftElbow_y = pose.key_points[6][1]
-                    # text.append(str(leftElbow_y))
-
-                    text.append(str("Right Wrist Y"))
                     rightWrist_y = pose.key_points[4][1]
-                    text.append(str(rightWrist_y))
-
-                    text.append(str("Left Wrist Y"))
                     leftWrist_y = pose.key_points[7][1]
-                    text.append(str(leftWrist_y))
-
-                    text.append(str("Right Hip Y"))
                     rightHip_y = post.key_points[8][1]
-                    text.append(str(rightHip_y))
-
-                    text.append(str("Left Hip Y"))
                     leftHip_y = post.key_points[11][1]
-                    text.append(str(leftHip_y))
 
-                if (rightWrist_y < rightShoulder_y) and (leftWrist_y < leftShoulder_y):
+                if (rightWrist_y < rightShoulder_y) or (leftWrist_y < leftShoulder_y):
                     text.append("Mood: Happy")
                 elif (rightWrist_y < rightHip_y) and  (leftWrist_y < leftHip_y):
                     text.append("Mood: Angry")
--- app_helen.py
+++ app_helen.py
@@ -13,28 +13,63 @@ Pose estimation is only supported using the edgeIQ container with an NCS
 accelerator.
 """
 
-
 def main():
-    image_paths = edgeiq.list_images(base_path="C:\Users\hanha\Documents\GitHub\hacksc2020\pic")
-    # Replace base_path with local image_path 
-    image = cv2.imread()
+    rightShoulder_y = 0
+    leftShoulder_y = 0
+
+    rightWrist_y = 0
+    leftWrist_y = 0
+
+    rightHip_y = 0
+    leftHip_y = 0
+
     pose_estimator = edgeiq.PoseEstimation("alwaysai/human-pose")
     pose_estimator.load(
-            engine = edgeiq.Engine.DNN_OPENVINO,
-            accelerator = edgeiq.Accelerator.MYRIAD)
+            engine=edgeiq.Engine.DNN_OPENVINO,
+            accelerator=edgeiq.Accelerator.MYRIAD)
 
-    # print("Loaded model:\n{}\n".format(pose_estimator.model_id))
-    # print("Engine: {}".format(pose_estimator.engine))
+    fps = edgeiq.FPS()
 
-    results = pose_estimator.estimate(image)
-    text = ["Model: {}".format(pose_estimator.model_id)]
-            for ind, pose in enumerate(results.poses):
-                text.append("Person {}".format(ind))
-                text.append('-'*10)
-                text.append("Key Points:")
-                for key_point in pose.key_points:
-                    text.append(str(key_point))
-                (results.draw_poses(image), text)
+    try:
+        with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
+                edgeiq.Streamer() as streamer:
+            time.sleep(2.0)
+            fps.start()
+
+            while True:
+                frame = video_stream.read()
+                results = pose_estimator.estimate(frame)
+
+                text = ["Model: {}".format(pose_estimator.model_id)]
+
+                # Get points
+                for ind, pose in enumerate(results.poses):
+                    rightShoulder_y = pose.key_points[2][1]
+                    leftShoulder_y = pose.key_points[5][1]
+                    rightWrist_y = pose.key_points[4][1]
+                    leftWrist_y = pose.key_points[7][1]
+                    rightHip_y = post.key_points[8][1]
+                    leftHip_y = post.key_points[11][1]
+
+                if (rightWrist_y < rightShoulder_y) or (leftWrist_y < leftShoulder_y):
+                    text.append("Mood: Happy")
+                elif (rightWrist_y < rightHip_y) and  (leftWrist_y < leftHip_y):
+                    text.append("Mood: Angry")
+                else:
+                    text.append("Mood: Idle")
+
+                streamer.send_data(results.draw_poses(frame), text)
+
+                fps.update()
+
+                if streamer.check_exit():
+                    break
+    finally:
+        fps.stop()
+        print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
+        print("approx. FPS: {:.2f}".format(fps.compute_fps()))
+
+        print("Program Ending")
 
 
 if __name__ == "__main__":
