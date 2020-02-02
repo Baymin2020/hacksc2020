b5f0428be4a939c2666dfb2995a6f01f5363786f
Kashyap Panda <kashyap.panda@gmail.com>
2020-02-01 18:23:58 -0800

    Added localized tracking

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 import logging
 import time
 import edgeiq
-import numpy as np
-"""
-Use pose estimation to determine human poses in realtime. Human Pose returns
-a list of key points indicating joints that can be used for applications such
-as activity recognition and augmented reality.
-
-Pose estimation is only supported using the edgeIQ container with an NCS
-accelerator.
-"""
 
 def main():
     pose_estimator = edgeiq.PoseEstimation("alwaysai/human-pose")
@@ -17,29 +8,23 @@ def main():
             engine=edgeiq.Engine.DNN_OPENVINO,
             accelerator=edgeiq.Accelerator.MYRIAD)
 
-    print("Loaded model:\n{}\n".format(pose_estimator.model_id))
-    print("Engine: {}".format(pose_estimator.engine))
-    print("Accelerator: {}\n".format(pose_estimator.accelerator))
-
     fps = edgeiq.FPS()
 
     try:
         with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
                 edgeiq.Streamer() as streamer:
-            # Allow Webcam to warm up
             time.sleep(2.0)
             fps.start()
 
-            # loop detection
             while True:
                 frame = video_stream.read()
                 results = pose_estimator.estimate(frame)
-                # Generate text to display on streamer
+
                 text = ["Model: {}".format(pose_estimator.model_id)]
 
+                # Get right shoulder points
                 for ind, pose in enumerate(results.poses):
-                    for key_point in pose.key_points:
-                        text.append(str(key_point))
+                    text.append(str(pose.key_points[2]))
 
                 streamer.send_data(results.draw_poses(frame), text)
 
